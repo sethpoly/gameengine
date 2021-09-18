d182a88645d8e4857bd0d9ff6653a509d71d6c5a
Seth Polyniak <emailspolyniak@gmail.com>
2021-09-18 10:18:46 -0400

    Complete SDL Delay

--- src/Game.cpp
+++ src/Game.cpp
@@ -75,8 +75,13 @@ void Game::Setup() {
 
 void Game::Update() {
     // TODO: IF we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
-    while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
-   
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame); 
+    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
+    
+    
+
     // Store the current frame time 
     millisecsPreviousFrame = SDL_GetTicks();
 
